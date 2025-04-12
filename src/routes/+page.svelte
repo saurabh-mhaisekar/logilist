b25868f787a7c892c07fe6d216bc8af98f18d6c0
Saurabh Mhaisekar <sourabh8044@gmail.com>
2025-04-12 14:57:23 +0530

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    greetMsg = "Hello, " + name;
+  }
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri + Svelte</h1>
+
+  <form class="row" onsubmit={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button type="submit" class="bg-blue-500 text-white p-2 rounded-md"
+      >Greet</button
+    >
+  </form>
+  <p class="text-red-500">{greetMsg}</p>
+</main>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+</style>
